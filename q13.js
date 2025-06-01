0556f00da24d391b881e83793969db6283484723
rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-06-01 17:12:42 +0530

    update

--- q13.js
+++ q13.js
@@ -1,33 +1,88 @@
-//creating table of orders with empid,iteams,....
-
 db.orders.insertMany([
   {
-    empId: ObjectId('68395b2c8d5c4261e66c4bd0'),
-    items: { "2": 1, "4": 5 },
-    orderValue: 780,
-    status: "Completed"
+    empId: ObjectId("683860a0490c4b45407cb256"),
+    items: { 1: 3, 3: 2 },
+    orderValue: 345,
+    status: "pending",
   },
   {
-    empId: ObjectId('68395b2c8d5c4261e66c4bd1'),
-    items: { "3": 2 },
-    orderValue: 260,
-    status: "Pending"
+    empId: ObjectId("683860a0490c4b45407cb256"),
+    items: { 3: 3, 2: 2 },
+    orderValue: 600,
+    status: "pending",
   },
-  {
-    empId: ObjectId('683983858d5c4261e66c4bd5'),
-    items: { "1": 1, "5": 2 },
-    orderValue: 510,
-    status: "Shipped"
-  }
-])
-//displaying all orders placed by employees
-db.orders.aggregate([
+]);
+
+db.employees.aggregate([
   {
     $lookup: {
-      from: "employees",
-      localField: "empId",
-      foreignField: "_id",
-      as: "employeeDetails"
-    }
-  }
-])
+      from: "orders",
+      localField: "_id",
+      foreignField: "empId",
+      as: "Ord",
+    },
+  },
+  { $project: { name: 1, "Ord.orderValue": 1 } },
+]);
+
+db.employees.aggregate([
+  {
+    $lookup: {
+      from: "orders",
+      localField: "_id",
+      foreignField: "empId",
+      as: "Ord",
+    },
+  },
+  { $unwind: "$Ord" },
+  { $project: { name: 1, "Ord.orderValue": 1 } },
+]);
+
+db.employees.aggregate([
+  {
+    $lookup: {
+      from: "orders",
+      localField: "_id",
+      foreignField: "empId",
+      as: "Ord",
+    },
+  },
+  {
+    $lookup: {
+      from: "address",
+      localField: "_id",
+      foreignField: "empId",
+      as: "Addr",
+    },
+  },
+]);
+
+db.employees.aggregate([
+  { $match: { email: "chastity@gmail.com" } },
+  {
+    $lookup: {
+      from: "orders",
+      localField: "_id",
+      foreignField: "empId",
+      as: "Ord",
+    },
+  },
+  { $unwind: "$Ord" },
+  {
+    $lookup: {
+      from: "address",
+      localField: "_id",
+      foreignField: "empId",
+      as: "Addr",
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      "Addr.residence.city": 1,
+      "Ord.orderValue": 1,
+      "Ord.status": 1,
+    },
+  },
+]);
